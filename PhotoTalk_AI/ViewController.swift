b3b75c71e4e27f4f957724a5152d2e3dd672fd5e
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-12-13 13:58:25 +0300

    Initial Commit

---
+++ PhotoTalk_AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoTalk_AI
+//
+//  Created by Samet Korkmaz on 13.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
