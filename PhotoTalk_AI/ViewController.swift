6b3d5051b11c4c69d586ba4ed4af881453f0abce
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-12-13 16:31:58 +0300

    PhotoTalk appICon and color sets setup

--- PhotoTalk_AI/ViewController.swift
+++ PhotoTalk_AI/ViewController.swift
@@ -9,12 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    private let view1: UIView = {
+        let view1 = UIView()
+        view1.layer.cornerRadius = 10
+        view1.layer.borderWidth = 10
+        view1.layer.borderColor = UIColor.second.cgColor
+        view1.backgroundColor = .main
+        return view1
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .bg
+        prepareUI()
+        
     }
 
+    func prepareUI() {
+        [view1].forEach{
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            view1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            view1.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            view1.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
+            view1.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            
+        ])
+    }
 
 }
 #Preview("UIKit"){
